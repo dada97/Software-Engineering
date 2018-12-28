da7874d1b657807e77476ba283fb363d95d85794
pe310186 <pe310186@gmail.com>
2018-12-28 14:23:22 +0800

    comment

---
+++ src/routes/commentRouter.js
@@ -0,0 +1,22 @@
+import Router from '../router.js'
+
+import CommentController from '../software-engineering/controllers/comment.js'
+
+class Comment extends Router {
+    constructor() {
+        super()
+        this.controller = new CommentController()
+        this.init()
+    }
+
+    init() {
+        this.get('/:id',     this.controller.getCommentById) //用article id取得留言
+        this.post('/:id',   this.controller.createComment)  //留言
+        this.put('/:id',    this.controller.upate)  //修改留言
+        this.delete(':/id', this.controller.deleteComment)  //刪除留言
+        
+    }
+}
+
+const CommentRouter = new Comment()
+export default CommentRouter.Match.bind(CommentRouter)
