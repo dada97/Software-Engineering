1e104a35f7dafe62ad9935a012e0e46e1f0398c0
dada97 <kerodada@gmail.com>
2018-12-28 13:57:22 +0800

    index.html

--- src/software-engineering/models/MySQL.js
+++ src/software-engineering/models/MySQL.js
@@ -1,5 +1,70 @@
+var mysql = require('mysql');
+
 export default class Model {
-    constructor(){
-        
+    constructor(table){
+        this.table =table;
+		this.connection= mysql.createConnection({
+		host: "localhost",
+		user: "root",
+		password: "1234",
+		port:3306,
+		database : 'mydb'
+		});
     }
+
+
+ addAccount(account,password,username,gender){
+	this.connection.connect(function(err) {
+	var myDate = new Date().toJSON().slice(0, 19).replace('T', ' ');
+	var sql = "INSERT INTO account (account,password,username,gender,createtime) VALUES ('"+account+"','"+password+"','"+username+"','"+gender+"','"+myDate+"')";
+		con.query(sql, function (err, result) {
+			if (err) throw err;
+			console.log("1 record inserted");
+		});
+		this.connection.end();
+	});
+}
+
+
+select(){
+	this.connection.connect(function(err) {
+		con.query("SELECT * FROM account_table", function (err, result, fields) {
+			if (err) throw err;
+			console.log(result);
+		});
+		this.connection.end();
+	});
+}
+
+where(col_name,target){
+	this.connection.connect(function(err) {
+		con.query("SELECT * FROM account_table WHERE " +col_name +" = '"+target+"'", function (err, result) {
+			if (err) throw err;
+			console.log(result);
+		});
+		this.connection.end();
+	});
+}
+delete_(col_name,target){
+	this.connection.connect(function(err) {
+		con.query("DELETE FROM account_table WHERE " +col_name +" = '"+target+"'", function (err, result) {	
+			if (err) throw err;
+			console.log("delete one record");
+		});
+		this.connection.end();
+	});
+}
+
+update(col_name,oldarg,newarg){
+	this.connection.connect(function(err) {
+		var sql = "UPDATE account_table SET "+col_name+" = '"+newarg+"' WHERE "+col_name+" = '"+oldarg+"'";
+		con.query(sql, function (err, result) {
+		if (err) throw err;
+		console.log(result.affectedRows + " record(s) updated");
+		});
+		this.connection.end();
+	});
+	
+}
+
 }
--- src/software-engineering/repositories/accountRepository.js
+++ src/software-engineering/repositories/accountRepository.js
@@ -7,27 +7,27 @@ export default class Account {
 
     //建立帳戶
     async createAccount(data){
-
+		return this.AccountModel.addAccount(account,password,username,gender);
     }
 
     //用帳號搜尋帳戶資訊
     async getAccountByAccount(account){
-        
+       return this.AccountModel.where("account",account);
     }
 
     //用id取得帳戶資訊
     async getAccountById(id){
-
+		return this.AccountModel.where("ID",id);
     }
 
     //取得全部帳戶資訊
     async getAllAccount(){
-
+		return this.AccountModel.select();
     }
 
     //刪除特定id帳戶
     async deleteAccountById(id){
-
+		return this.AccountModel.delete_("ID",id);
     }
 
     //更新帳戶資訊
@@ -37,7 +37,7 @@ export default class Account {
 
     //用userName取得帳戶資訊
     async getAccountByName(name){
-
+		return this.AccountModel.where("username",name);
     }
 
     //搜尋
