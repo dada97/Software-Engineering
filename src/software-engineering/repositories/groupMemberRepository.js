6b975158798da86d235f64cde52dee15d0edfd9d
dada97 <kerodada@gmail.com>
2019-01-03 17:45:50 +0800

    back

---
+++ src/software-engineering/repositories/groupMemberRepository.js
@@ -0,0 +1,21 @@
+import Model from '../models/MySQL'
+
+export default class Group {
+    constructor() {
+        this.GroupMemberModel = new Model('groupmember')
+    }
+	
+	async getGroupMemberByGroupID(id){
+        return await this.GroupMemberModel.where(this.GroupMemberModel.table,"groupID",id);
+    }
+
+    async join(data){
+        console.log(data)
+        return await this.GroupMemberModel.insert(this.GroupMemberModel.table,data);
+    }
+
+    async quit(id){
+        return await this.GroupMemberModel.delete_(this.GroupMemberModel.table,"ID",id);
+    }
+    
+}
